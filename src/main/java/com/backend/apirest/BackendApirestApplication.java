1c6768c6a002b55c89cf59614cb531bca02bedba
Pablo Hernandez <pablo.hdez.v@outlook.com>
2021-04-04 23:37:30 -0500

    agregando swagger

--- src/main/java/com/backend/apirest/BackendApirestApplication.java
+++ src/main/java/com/backend/apirest/BackendApirestApplication.java
@@ -2,12 +2,29 @@ package com.backend.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BackendApirestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApirestApplication.class, args);
+	}
+
+	@Bean
+	public Docket productsApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 
 }
